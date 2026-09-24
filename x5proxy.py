ecedd9a2f4b6c28d0826913d065a02a91a1046db
kimo <lava4ggaming@gmail.com>
2026-09-25 01:48:14 +0300

    chore: keep v1.0.1 version (in-place release update)

--- x5proxy.py
+++ x5proxy.py
@@ -33,7 +33,7 @@ import urllib.error
 import zipfile
 
 APP_NAME = "IPNET"
-APP_VERSION = "v1.0.2"
+APP_VERSION = "v1.0.1"
 CANONICAL_REPO = "X5Coder/proxy-usa"  # templates are downloaded from here
 PROJECT_FILES = [
     "server.py",
